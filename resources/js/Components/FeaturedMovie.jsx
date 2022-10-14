9e7d4b8f00e8664b081aa5664118f844bc6d7095
rahmadhany98 <kazuha004@gmail.com>
2022-10-14 18:16:22 +0700

    feat: slicing detail movie

--- resources/js/Components/FeaturedMovie.jsx
+++ resources/js/Components/FeaturedMovie.jsx
@@ -1,4 +1,5 @@
 import PropType from "prop-types"
+import { Link } from "@inertiajs/inertia-react"
 
 FeaturedMovie.propTypes = {
     slug: PropType.string.isRequired,
@@ -37,7 +38,7 @@ export default function FeaturedMovie({
                 <img src="/icons/ic_play.svg" width="50" alt="" />
             </div>
         </div>
-        <a href={slug} className="inset-0 absolute z-50"></a>
+        <Link href={route('prototype.movie.show', slug)} className="inset-0 absolute z-50"></Link>
     </div>
     )
 }
